ded5e487a1099f132412090dddcc4cef20faeb9e
Jerrythafast <2095375+Jerrythafast@users.noreply.github.com>
2020-05-13 22:55:05 +0200

    Fix typo in liveupdate.py

--- opt/se-logger/liveupdate.py
+++ opt/se-logger/liveupdate.py
@@ -205,7 +205,7 @@ class SEParser:
         elif state == 8:
           # Reading message checksum.
           if len(data) == 20 + length + 2:
-            data = byterray(data)
+            data = bytearray(data)
             hdr = struct.unpack("<LHHHLLH", data[:20])
             # Check the checksum.
             if struct.unpack("<H", data[-2:])[0] != calcCrc(
